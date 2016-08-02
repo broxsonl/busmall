d8f04df3ed6f7942408a12fb8c1fd8e04f22493e
Lee Broxson <broxsonl@gmail.com>
2016-08-01 18:19:50 -0700

    Got random images and clicks for images to work.

--- js/app.js
+++ js/app.js
@@ -4,6 +4,13 @@
 
 var catalogArray = [];
 
+//Event Listener Global
+var userClick = document.getElementById('picSection');
+
+var leftImg = document.getElementById('left');
+var centerImg = document.getElementById('center');
+var rightImg = document.getElementById('right');
+
 //catalogItem constructor
 function CatalogItem (imageName, filePath) {
   this.imageName = imageName;
@@ -14,23 +21,79 @@ function CatalogItem (imageName, filePath) {
   catalogArray.push(this);
 }
 
-new CatalogItem ('R2D2 Bag', '../img/bag.jpg');
-new CatalogItem ('Banana Slicer', '../img/banana.jpg');
-new CatalogItem ('iPad TP Stand', '../img/bathroom.jpg');
-new CatalogItem ('Open-toed Boots', '../img/boots.jpg');
-new CatalogItem ('All-in-One Breaky', '../img/breakfast.jpg');
-new CatalogItem ('Meatball Gum', '../img/bubblegum.jpg');
-new CatalogItem ('Chair', '../img/chair.jpg');
-new CatalogItem ('Cthulhu', '../img/cthulhu.jpg');
-new CatalogItem ('Dog Duck Bill', '../img/dog-duck.jpg');
-new CatalogItem ('Dragon Meat', '../img/dragon.jpg');
-new CatalogItem ('Utensil Pen', '../img/pen.jpg');
-new CatalogItem ('Pet Sweeper', '../img/pet-sweep.jpg');
-new CatalogItem ('Pizza Scissors', '../img/scissors.jpg');
-new CatalogItem ('Shark Sleeper', '../img/shark.jpg');
-new CatalogItem ('Baby Sweeper', '../img/sweep.jpg');
-new CatalogItem ('Tauntaun Sleeper', '../img/tauntaun.jpg');
-new CatalogItem ('Unicorn Meat', '../img/unicorn.jpg');
-new CatalogItem ('Tentacle USB', '../img/usb.jpg');
-new CatalogItem ('Watering Can', '../img/water-can.jpg');
-new CatalogItem ('Wine Glass', '../img/wine-glass.jpg');
+//CatalogItems created here
+new CatalogItem ('R2D2 Bag', 'img/bag.jpg');
+new CatalogItem ('Banana Slicer', 'img/banana.jpg');
+new CatalogItem ('iPad TP Stand', 'img/bathroom.jpg');
+new CatalogItem ('Open-toed Boots', 'img/boots.jpg');
+new CatalogItem ('All-in-One Breaky', 'img/breakfast.jpg');
+new CatalogItem ('Meatball Gum', 'img/bubblegum.jpg');
+new CatalogItem ('Chair', 'img/chair.jpg');
+new CatalogItem ('Cthulhu', 'img/cthulhu.jpg');
+new CatalogItem ('Dog Duck Bill', 'img/dog-duck.jpg');
+new CatalogItem ('Dragon Meat', 'img/dragon.jpg');
+new CatalogItem ('Utensil Pen', 'img/pen.jpg');
+new CatalogItem ('Pet Sweeper', 'img/pet-sweep.jpg');
+new CatalogItem ('Pizza Scissors', 'img/scissors.jpg');
+new CatalogItem ('Shark Sleeper', 'img/shark.jpg');
+new CatalogItem ('Baby Sweeper', 'img/sweep.jpg');
+new CatalogItem ('Tauntaun Sleeper', 'img/tauntaun.jpg');
+new CatalogItem ('Unicorn Meat', 'img/unicorn.jpg');
+new CatalogItem ('Tentacle USB', 'img/usb.jpg');
+new CatalogItem ('Watering Can', 'img/water-can.jpg');
+new CatalogItem ('Wine Glass', 'img/wine-glass.jpg');
+
+var randomIndex1 = Math.floor(Math.random() * catalogArray.length);
+var randomIndex2 = Math.floor(Math.random() * catalogArray.length);
+var randomIndex3 = Math.floor(Math.random() * catalogArray.length);
+
+function loadImages() {
+
+  var randomIndex1 = Math.floor(Math.random() * catalogArray.length);
+  var randomIndex2 = Math.floor(Math.random() * catalogArray.length);
+  var randomIndex3 = Math.floor(Math.random() * catalogArray.length);
+
+  while (randomIndex2 === randomIndex1) {
+    randomIndex2 = Math.floor(Math.random() * catalogArray.length);
+  }
+
+  while (randomIndex3 === randomIndex2 || randomIndex3 === randomIndex1) {
+    randomIndex3 = Math.floor(Math.random() * (catalogArray.length));
+  }
+
+  leftImg.src = catalogArray[randomIndex1].filePath;
+  catalogArray[randomIndex1].tallyDisplayed += 1;
+  centerImg.src = catalogArray[randomIndex2].filePath;
+  catalogArray[randomIndex2].tallyDisplayed += 1;
+  rightImg.src = catalogArray[randomIndex3].filePath;
+  catalogArray[randomIndex3].tallyDisplayed += 1;
+
+}
+
+function handleUserClick(event) {
+  event.preventDefault();
+
+  alert('Click is working');
+
+  if (event.target.id === 'left') {
+    catalogArray[randomIndex1].tallyClicked += 1;
+  }
+
+  else if (event.target.id === 'center') {
+    catalogArray[randomIndex2].tallyClicked += 1;
+  }
+
+  else if (event.target.id === 'right') {
+    catalogArray[randomIndex3].tallyClicked += 1;
+  }
+  else {
+    alert('Pick a product!');
+  }
+
+  // console.log(clickedObject)
+  loadImages();
+}
+
+loadImages();
+
+userClick.addEventListener('click', handleUserClick);
